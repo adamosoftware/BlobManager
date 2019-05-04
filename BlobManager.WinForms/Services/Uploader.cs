53ab20b698bf41d47b33fcd6bdc0ee3679734d6b
Adam O'Neil <adamosoftware@gmail.com>
2019-05-04 12:10:23 -0400

    started on main form

---
+++ BlobManager.WinForms/Services/Uploader.cs
@@ -0,0 +1,29 @@
+﻿using JsonSettings;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace BlobManager.WinForms.Services
+{
+	public class Uploader
+	{
+		/// <summary>
+		/// Local folder where files are uploaded from
+		/// </summary>
+		public string SourceFolder { get; set; }
+
+		[JsonProtect(DataProtectionScope.CurrentUser)]
+		public string TargetAccountName { get; set; }
+
+		[JsonProtect(DataProtectionScope.CurrentUser)]
+		public string TargetAccountKey { get; set; }
+
+		[JsonProtect(DataProtectionScope.CurrentUser)]
+		public string TargetContainer { get; set; }
+
+
+		public async Task UploadFilesAsync()
+		{
+
+		}
+	}
+}
--- BlobManager.WinForms/frmMain.Designer.cs
+++ BlobManager.WinForms/frmMain.Designer.cs
@@ -28,43 +28,118 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.fileGridView1 = new BlobManager.WinForms.Controls.FileGridView();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.fgvLocal = new BlobManager.WinForms.Controls.FileGridView();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+			this.fgvRemote = new BlobManager.WinForms.Controls.FileGridView();
+			this.tbLocalFolder = new System.Windows.Forms.ToolStripTextBox();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// button1
+			// splitContainer1
 			// 
-			this.button1.Location = new System.Drawing.Point(128, 43);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
 			// 
-			// fileGridView1
+			// splitContainer1.Panel1
 			// 
-			this.fileGridView1.Location = new System.Drawing.Point(128, 72);
-			this.fileGridView1.Name = "fileGridView1";
-			this.fileGridView1.Size = new System.Drawing.Size(593, 263);
-			this.fileGridView1.TabIndex = 2;
+			this.splitContainer1.Panel1.Controls.Add(this.fgvLocal);
+			this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.fgvRemote);
+			this.splitContainer1.Panel2.Controls.Add(this.toolStrip2);
+			this.splitContainer1.Size = new System.Drawing.Size(773, 357);
+			this.splitContainer1.SplitterDistance = 384;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// fgvLocal
+			// 
+			this.fgvLocal.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.fgvLocal.Location = new System.Drawing.Point(0, 25);
+			this.fgvLocal.Name = "fgvLocal";
+			this.fgvLocal.Size = new System.Drawing.Size(384, 332);
+			this.fgvLocal.TabIndex = 0;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbLocalFolder,
+            this.toolStripButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(384, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStrip2
+			// 
+			this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip2.Name = "toolStrip2";
+			this.toolStrip2.Size = new System.Drawing.Size(385, 25);
+			this.toolStrip2.TabIndex = 0;
+			this.toolStrip2.Text = "toolStrip2";
+			// 
+			// fgvRemote
+			// 
+			this.fgvRemote.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.fgvRemote.Location = new System.Drawing.Point(0, 25);
+			this.fgvRemote.Name = "fgvRemote";
+			this.fgvRemote.Size = new System.Drawing.Size(385, 332);
+			this.fgvRemote.TabIndex = 1;
+			// 
+			// tbLocalFolder
+			// 
+			this.tbLocalFolder.Name = "tbLocalFolder";
+			this.tbLocalFolder.Size = new System.Drawing.Size(300, 25);
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(51, 22);
+			this.toolStripButton1.Text = "Select...";
 			// 
 			// frmMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.fileGridView1);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(773, 357);
+			this.Controls.Add(this.splitContainer1);
 			this.Name = "frmMain";
 			this.Text = "Form1";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-		private System.Windows.Forms.Button button1;
-		private Controls.FileGridView fileGridView1;
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private Controls.FileGridView fgvLocal;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripTextBox tbLocalFolder;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private Controls.FileGridView fgvRemote;
+		private System.Windows.Forms.ToolStrip toolStrip2;
 	}
 }
 
