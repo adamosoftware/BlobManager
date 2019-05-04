f0c7fcda005d563bfb4af59bd77a3a247284e0c4
Adam O'Neil <adamosoftware@gmail.com>
2019-05-04 11:02:10 -0400

    removed FileListView stuff

--- BlobManager.WinForms/frmMain.cs
+++ BlobManager.WinForms/frmMain.cs
@@ -1,13 +1,7 @@
-﻿using BlobManager.WinForms.Controls;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BlobManager.WinForms
